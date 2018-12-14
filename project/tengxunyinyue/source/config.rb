a629ae9098966e42e06f00a7628591618c2ac671
Yicoding <804413572@qq.com>
2018-12-14 14:51:13 +0800

    add

---
+++ project/tengxunyinyue/source/config.rb
@@ -0,0 +1,12 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+
+relative_assets = true
+line_comments = false
+enable_sourcemaps = true
+sass_options = {:sourcemap => true}
+
+output_style = :compressed
+environment = :product
